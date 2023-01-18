4c2dc46513a0614135dfbd8cc2d2c3c9a41e8151
SaintlyTea <78848975+SaintlyTea@users.noreply.github.com>
2023-01-18 11:34:53 +0100

    Add files via upload

---
+++ src/main/scala/Integers.scala
@@ -0,0 +1,111 @@
+import scala.util.control.Exception
+// test haha
+object Integers {
+    val constant = 42
+
+    def main(args:Array[String]):Unit = {
+        Factional(1)
+    }
+
+    /**
+     * add 1 to number x
+     */
+    def Increment(x:Int): Int =
+        x + 1
+
+    /**
+     * if bigger than constant: true
+     */
+    def BiggerThanConstant(input:Int):Boolean =
+        input > constant
+
+    /**
+     * Gets square of x
+     */
+    def Square(x:Int):Int =
+        x*x
+
+    /**
+     * x plus y
+     */
+    def Max(x:Int, y:Int):Int = {
+        if (x > y)
+            return x
+        y
+    }
+
+    /**
+     * get absolute of x
+     */
+    def Abs(x:Int):Int = {
+        if (x < 0)
+            return x * -1
+        x
+    }
+
+    /**
+     * Rest of Division for x divided by y
+     */
+    def ReturnRest(x:Int, y:Int):Int =
+        x-((x/y)*y)
+
+    /**
+     * sum of all numbers until n
+     * REQUIRES: n>=0
+     */
+    def Factional(n:Int):Int = {
+        if (n > 0) {
+            var sum = 0
+            for (i <- 0  to n )
+                sum += i
+            return sum
+        }
+        // if Factional(0) return 1
+        1
+    }
+
+    /**
+     * Return result of exponent
+     */
+    def POWEEEERRR(num:Int, exponent:Int):Int = {
+        var result = 0
+
+        if (exponent == 0)
+            result = 1
+        else if (num != 0) {
+            result = num
+            for (i <- 1 until exponent)
+                result *= num
+        }
+        result
+    }
+
+    def SumOfEachSquaredDigitInNumber(n:Int):Int = {
+        var result = 0
+
+        var numberAsString = n.toString
+        numberAsString = numberAsString.replace("-", "")
+
+        numberAsString.foreach(num =>
+            result += num.asDigit * num.asDigit)
+         result
+    }
+
+    def Inverse(x:Int, m:Int):Int = {
+        var result = 0
+
+        if (x > m)
+            return 0
+
+        var i = 1
+        while (!(result == i)){
+            if (ReturnRest(x*i, m) == 1)
+                result = i
+            else
+                i += 1
+            if (i >= 1000000)
+                throw new Exception("Counted until 1000000 and no match found")
+        }
+        result
+    }
+}
---
+++ src/test/scala/IntegersTest.scala
@@ -0,0 +1,63 @@
+import org.scalatest.FunSuite
+import Integers._
+class IntegersTest extends FunSuite {
+    test( "Increment"){
+      assert(Increment(9) == 10)
+    }
+
+  test("Abs"){
+    assert(Abs(-10) == 10)
+    assert(Abs(10) == 10)
+  }
+
+    test("Test für die ReturnRest-function"){
+      assert(ReturnRest(13, 3) == 1)
+      assert(ReturnRest(25, 7) == 4)
+      assert(ReturnRest(0, 1) == 0)
+      assert(ReturnRest(1, 1) == 0)
+      assert(ReturnRest(1, 2) == 1)
+
+      assert(ReturnRest(1, -2) == 1)
+      assert(ReturnRest(-1, 2) == -1)
+      assert(ReturnRest(-1, -2) == -1)
+    }
+
+  test("Test of function BiggerThanConstant"){
+    // true
+    assert(BiggerThanConstant(50))
+
+    // false
+    assert(!BiggerThanConstant(constant))
+    assert(!BiggerThanConstant(1))
+  }
+
+  test("SumUpTo"){
+    // success
+    assert(Factional(0) == 1)
+    assert(Factional(1) == 1)
+    assert(Factional(4) == 10)
+    assert(Factional(10) == 55)
+  }
+
+  test("POWEEEERRR"){
+    assert(POWEEEERRR(0, 4) == 0)
+    assert(POWEEEERRR(2, 0) == 1)
+    assert(POWEEEERRR(3, 2) == 9)
+    assert(POWEEEERRR(2, 4) == 16)
+  }
+
+  test("SumOfEachSquaredDigitInNumber"){
+    assert(SumOfEachSquaredDigitInNumber(0) == 0)
+    assert(SumOfEachSquaredDigitInNumber(1) == 1)
+    assert(SumOfEachSquaredDigitInNumber(2) == 4)
+    assert(SumOfEachSquaredDigitInNumber(139) == 91)
+
+    assert(SumOfEachSquaredDigitInNumber(-3) == 9)
+    assert(SumOfEachSquaredDigitInNumber(-139) == 91)
+  }
+
+  test("inverse"){
+    assert(Inverse(1, 3) == 1)
+    assert(Inverse(1, 3) == 1)
+  }
+}
