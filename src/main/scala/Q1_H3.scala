5acd7b32621c7ae8d2165339d9b5a87c5a082592
SaintlyTea <jojann.alberts@gmail.com>
2023-02-01 11:55:51 +0100

    added task 26 and 27

--- src/main/scala/Q1_H3.scala
+++ src/main/scala/Q1_H3.scala
@@ -1,5 +1,5 @@
 import scala.annotation.tailrec
-
+import scala.language.postfixOps
 object Q1_H3 {
 
   /**
@@ -17,7 +17,7 @@ object Q1_H3 {
       else if (x > y)
         1
       else if (x == 0)
-        Production(x + 1, (x + y) / 2) * Production((x + y) / 2 + 1, y)
+        1 * Production(x + 1, (x + y) / 2) * Production((x + y) / 2 + 1, y)
       else
         x * Production(x + 1, (x + y) / 2) * Production((x + y) / 2 + 1, y)
     }
@@ -26,8 +26,7 @@ object Q1_H3 {
   }
 
   /**
-   *
-   * @note Make this into recursive
+   * this is the Fibonacci but end-recursive
    * @param n : Int
    * @return
    */
@@ -45,4 +44,55 @@ object Q1_H3 {
   def PressF(): Int = {
     0
   }
+
+  // Start of task 26 and 27
+
+  /**
+   * returns opposite of x
+   * @param x:Boolean
+   * @return
+   */
+  def Not(x:Boolean):Boolean = !x
+
+  /**
+   * return true if only one is true
+   * @param x:Boolean
+   * @param y:Boolean
+   * @return
+   */
+  def Xor(x:Boolean, y:Boolean):Boolean = if(x == y) false else true
+
+  /**
+   * x implies y
+   * @param x:Boolean
+   * @param y:Boolean
+   * @return
+   */
+  def Implies(x:Boolean, y:Boolean):Boolean = if (x && !y) false else true
+
+  /**
+   * are equal
+   * @param x:Boolean
+   * @param y:Boolean
+   * @return
+   */
+  def Equivalent(x:Boolean, y:Boolean):Boolean = if (x == y) true else false
+
+  /**
+   * makes character into the Unicide number
+   * @param c:Char
+   * @return
+   */
+  def ToUnicode(c:Char):Int = c
+
+  /**
+   * makes number of unicode to the character
+   * @param x:Int
+   * @return
+   */
+  def ToCharacter(x:Int):Char = x.toChar
+
+  // End of task 26 and 27
+
+
 }
--- src/test/scala/Q1_H3_Test.scala
+++ src/test/scala/Q1_H3_Test.scala
@@ -1,19 +1,57 @@
 import Q1_H3._
 import org.scalatest.FunSuite
 
+//noinspection NameBooleanParameters
 class Q1_H3_Test extends  FunSuite {
   test("Production"){
-    assert(Product(0,6) == 0)
+    assert(Product(0,6) == 120)
+    assert(Product(1,6) == 120)
     assert(Product(3,6) == 60)
+    assert(Product(10,11) == 10)
     assert(Product(9, 15) == 2162160)
     assert(Product(10, 30) == BigInt("24365525776399090483200000"))
   }
 
   test("Fibonacci"){
     assert(Fibonacci(4) == 3)
+    assert(Fibonacci(5) == 5)
     assert(Fibonacci(6) == 8)
     assert(Fibonacci(50) == 12586269025L)
+
+    // H24 is true
+    var test:BigInt = 0
+    for (i <- 1 to 8) {
+      test += Fibonacci(i)
+    }
+    assert(test == Fibonacci(8+2)-1)
   }
 
+  test("Not"){
+    assert(!Not(true))
+    assert(Not(false))
+  }
 
+  test("Xor"){
+    assert(!Xor(true, true))
+    assert(Xor(true, false))
+  }
+
+  test("Implies"){
+    assert(Implies(true, true))
+    assert(Implies(false, false))
+    assert(!Implies(true, false))
+  }
+
+  test("Equivalent"){
+    assert(Equivalent(true, true))
+    assert(!Equivalent(true, false))
+  }
+
+  test("ToUnicode"){
+    assert(ToUnicode('a') == 97)
+  }
+
+  test("ToCharacter"){
+    assert(ToCharacter(97) == 'a')
+  }
 }
