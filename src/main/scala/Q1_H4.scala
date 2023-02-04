2d1cfbcb2df610138ffda6986f7eba8a73d575bf
SaintlyTea <jojann.alberts@gmail.com>
2023-02-04 15:16:08 +0100

    added test for SwapAt and added MyToString

--- src/main/scala/Q1_H4.scala
+++ src/main/scala/Q1_H4.scala
@@ -3,14 +3,33 @@ import Q1_H3._
 import scala.annotation.tailrec
 
 object Q1_H4 {
+
+  /**
+   * swapt a string from where the character says
+   * @param s:String
+   * @param c:Char
+   * @return
+   */
   def SwapAt(s:String, c:Char): String = {
     @tailrec
     def SwapAtER(stringy:String, Charaptos:Char):String = {
-      if (MyHead(stringy) == Charaptos || MyHead(stringy).toString == "")
+      if (MyHead(stringy) == Charaptos || MyToString(MyHead(stringy)) == "")
         MyTail(stringy)
       else
         SwapAtER(MyTail(stringy), c)
     }
      SwapAtER(s, c) + c + StringReverse(SwapAtER(StringReverse(s), c))
   }
+
+  /**
+   * Following methods are to convert different types of values to strings
+   * @return
+   */
+    def MyToString(number:Int):String = "" + number
+    def MyToString(booly:Boolean):String = "" + booly
+    def MyToString(stringy:String):String = stringy
+    def MyToString(chary:Char):String = "" + chary
+
+  // End of MyToString
+
 }
--- src/test/scala/Q1_H4_Tests.scala
+++ src/test/scala/Q1_H4_Tests.scala
@@ -6,4 +6,18 @@ class Q1_H4_Tests extends FunSuite {
   test("SwapAt"){
     assert(SwapAt("Hello there", ' ') == "there Hello")
   }
+
+  test("MyToString"){
+    assert(MyToString(true) == "true")
+    assert(MyToString(false) == "false")
+
+    assert(MyToString('t') == "t")
+    assert(MyToString('e') == "e")
+
+    assert(MyToString(4) == "4")
+    assert(MyToString(44) == "44")
+
+    assert(MyToString("Hello there") == "Hello there")
+    assert(MyToString("2b||Not(2b)") == "2b||Not(2b)")
+  }
 }
